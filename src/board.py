fab2ab8cfb57bb6920307c52434c73f1c81eaa8e
LeeoSilva <leeosilva0909@gmail.com>
2022-05-31 01:17:47 -0300

    refactored board.py class

--- src/board.py
+++ src/board.py
@@ -1,68 +1,154 @@
+import random
+
 import numpy as np
+
+from src.constants import BOARD_COLS, BOARD_ROWS, Player, Result
+from src.utils import get_max_move_count
 
 
 class Board:
     state: np.ndarray
-    winner: str
+    player: Player
+    move_count: int
+    move_history: list
+    game_result: Result
 
-    def __init__(self, state: list = np.full((3, 3), "_")) -> None:
-        self.state = state
-        self.winner = ""
+    def __init__(
+        self, state: np.ndarray = np.zeros((BOARD_ROWS, BOARD_COLS), dtype=int)
+    ) -> None:
+        self.state = np.copy(state)
+        self.player = Player.X  # By default the first player is X
+        self.move_count = 0
+        self.move_history = []
+        self.game_result = None
 
-    def evaluate(self) -> None:
-        """Should evaluate if the board is valid or not.
-        e.g.: A board with the following state is invalid:
-                X | X | X
-                X | O | X
-                O | O | O
-        """
-        pass
+        self.update_move_count()
 
-    def set_winner(self, winner: str) -> None:
-        if ["X", "O"] not in winner:
-            raise ValueError("The winner can only be X or O.")
+    def update_move_count(self) -> None:
+        x_move_count: int = np.count_nonzero(self.state == Player.X)
+        o_move_count: int = np.count_nonzero(self.state == Player.O)
 
-        self.winner = winner
+        self.move_count = x_move_count + o_move_count
 
-    def get_winner(self) -> str:
-        return self.winner
+    def check_stalemate(self) -> None:
+        if self.move_count >= get_max_move_count():
+            self.game_result = Result.STALEMATE
 
-    def get_state(self):
+    def make_move(self, move: int) -> None:
+        self.move_contraint(move)
+        coord = np.unravel_index(move, (BOARD_ROWS, BOARD_COLS))
+        self.state[coord] = int(self.player)
+        self.move_count += 1
+        self.move_history.append(move)
+        self.next_player()
+        self.is_game_over()
+
+    def move_contraint(self, move) -> None:
+        if self.is_game_over():
+            raise AttributeError("Game is already over.")
+
+        if move not in self.get_available_moves():
+            raise AttributeError(f"Move {move} is not an available move!")
+
+        if move not in range(0, 10):
+            raise AttributeError(f"Move `{move}` is an Ilegal move!")
+
+    def get_available_moves(self) -> list:
+        coords = np.where(self.state == Player.EMPTY)
+        coords = np.ravel_multi_index(coords, (BOARD_ROWS, BOARD_COLS))
+        return coords.tolist()
+
+    def fetch_game_result(self) -> Result:
+        return self.game_result
+
+    def undo_move(self) -> None:
+        coords = self.move_history[-1]
+        coords = np.unravel_index(coords, (BOARD_ROWS, BOARD_COLS))
+        del self.move_history[-1]
+
+        self.state[coords] = Player.EMPTY
+        self.game_result = None
+        self.move_count -= 1
+        self.next_player()
+
+    def set_game_result(self, winner: Player) -> None:
+        if winner in Player:
+            raise AttributeError(f"Winner `{winner}` is an invalid player.")
+
+        if winner is Player.EMPTY:
+            raise AttributeError(f"Winner cannot be an empty player.")
+
+        if winner == Player.X:
+            return self.x_won()
+
+        return self.o_won()
+
+    def x_won(self) -> None:
+        self.game_result = Result.X_WON
+
+    def o_won(self) -> None:
+        self.game_result = Result.O_WON
+
+    def stalemate(self) -> None:
+        self.game_result = Result.STALEMATE
+
+    def get_state(self) -> list:
         return self.state.tolist()
 
-    def draw(self) -> None:
+    def render(self) -> None:
         for row in self.state:
             for item in row:
                 print(f"| {item} |", end=" ")
             print("")
 
-    def check_win(self) -> str:
-        self.evaluate()
+    def is_game_over(self) -> bool:
+        self.check_stalemate()
 
-        self.check_horizontal_for_win()
-        self.check_vertical_for_win()
-        self.check_primary_diagonal_for_win()
-        self.check_secondary_diagonal_for_win()
+        self.check_horizontal()
+        self.check_vertical()
+        self.check_diagonal()
+        self.check_anti_diagonal()
 
-        return self.get_winner()
+        return self.fetch_game_result() is not None
 
-    def check_primary_diagonal_for_win(self) -> None:
+    def check_diagonal(self) -> None:
         diagonal = self.state.diagonal()
-        if np.all(diagonal == diagonal[0]):
-            self.winner = diagonal[0]
+        if self.check_three(diagonal):
+            return self.set_game_result(diagonal[0])
 
-    def check_secondary_diagonal_for_win(self) -> None:
-        secundary_diagonal = np.diag(np.fliplr(self.state))
-        if np.all(secundary_diagonal == secundary_diagonal[0]):
-            self.winner = secundary_diagonal[0]
+    def check_anti_diagonal(self) -> None:
+        anti_diagonal = np.diag(np.fliplr(self.state))
+        if self.check_three(anti_diagonal):
+            return self.set_game_result(anti_diagonal[0])
 
-    def check_horizontal_for_win(self) -> None:
+    def check_horizontal(self) -> None:
         for row in self.state:
-            if np.all(row == row[0]):
-                self.winner = row[0]
+            if self.check_three(row):
+                return self.set_game_result(row[0])
 
-    def check_vertical_for_win(self) -> None:
+    def check_vertical(self) -> None:
         for idx, _ in enumerate(self.state):
             vertical = self.state[:, idx]
-            if np.all(vertical == vertical[0]):
-                self.winner = vertical[0]
+            if self.check_three(vertical):
+                return self.set_game_result(vertical[0])
+
+    def check_three(self, combination) -> bool:
+        return combination[0] != Player.EMPTY and np.all(combination == combination[0])
+
+    def next_player(self) -> None:
+        """Handles next player logic."""
+        if self.player == Player.X:
+            return self.o_turn()
+
+        return self.x_turn()
+
+    def x_turn(self) -> None:
+        self.player = Player.X
+
+    def o_turn(self) -> None:
+        self.player = Player.O
+
+    def _random_player_starts(self) -> None:
+        """Gets a random player to start the game."""
+        players = [Player.X, Player.O]
+        self.player = random.choise(players)
